3b77004cbba5049629d3856599d9f988f3b90fb5
Kevin Gifford <ksgifford@gmail.com>
2016-02-01 23:40:47 -0800

    Initial commit to upload all files for first assignment submission.

---
+++ scripts/projList.js
@@ -0,0 +1,18 @@
+var projList = [
+  {
+    title:        'Donut Shop Reporting Tool',
+    category:     'Educational',
+    client:       'Code Fellows Foundations 1',
+    completeDate: '2015-05-25',
+    webLink:      'https://gist.github.com/ksgifford/2cfadb073801eb9f1229',
+    body:         '<p>Produced as a final project for the Code Fellows Foundations 1 course, this app uses jQuery to populate an HTML table with production data for a fictional chain of donut stores. The app also calculates the average production per hour for each store, based on their individual operating hours, which are adjustable.</p>'
+  },
+  {
+    title:        'Band Name Generator App',
+    category:     'Educational',
+    client:       'Code Fellows Foundations 2 - Javascript',
+    completeDate: '2015-10-15',
+    webLink:      'https://cf2-lab3-band-name-generator.herokuapp.com/',
+    body:         '<p>Produced as a final project for the Code Fellows Foundations 2 Javascript course, this app uses jQuery and Node.js to provide the user with a random name for their new garage band. Each band name contains one adjective, one verb, and one noun, and each term is queried from an associated HTML server. The app also accepts user input for other suggested names and check the submissions against the list of possible name components stored on the server.</p>'
+  },
+];
---
+++ scripts/projects.js
@@ -0,0 +1,42 @@
+var projects = [];
+
+function Project(options) {
+  this.title = options.title;
+  this.category = options.category;
+  this.client = options.client;
+  this.completeDate = options.completeDate;
+  this.webLink = options.webLink;
+  this.body = options.body;
+}
+
+Project.prototype.toHtml = function() {
+  var $newProject = $('article.template').clone();
+
+  $newProject.attr('category', this.category);
+  $newProject.find('header h2').text(this.title);
+  $newProject.find('.prjDesc').html(this.body);
+  $newProject.find('.prjInfo a').text(this.client);
+  $newProject.find('.prjInfo a').attr('href', this.webLink);
+
+  $newProject.find('time[datetime]').text(this.completeDate);
+  $newProject.find('time[pubdate]').attr('title', this.completeDate);
+  $newProject.find('time').html('about ' + parseInt((new Date() - new Date(this.completeDate))/60/60/24/1000) + ' days ago');
+
+  $newProject.append('<br></br>');
+
+  $newProject.removeClass('template');
+
+  return $newProject;
+}
+
+projList.sort(function(a,b) {
+  return (new Date(b.completeDate)) - (new Date(a.completeDate));
+});
+
+projList.forEach(function(ele) {
+  projects.push(new Project(ele));
+});
+
+projects.forEach(function(a){
+  $('#projects').append(a.toHtml())
+});

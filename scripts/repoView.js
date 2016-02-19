22b2ecc1190320ec0e9cc75e5e48bf3d717c57fe
Kevin Gifford <ksgifford@gmail.com>
2016-02-18 23:59:26 -0800

    Revisions to incorporate ctx object state management. Not fully functional.

--- scripts/repoView.js
+++ scripts/repoView.js
@@ -1,10 +1,11 @@
 (function(module) {
   var repoView = {};
 
-  repoView.index = function() {
-    $('#about ul').empty();
-    $('#about ul').append(
-      repos.with('description').map(repos.toHtml));
+  repoView.index = function(repoList) {
+    $('#repo-list').empty();
+    $('#repo-list').append(
+      repoList.map(repos.toHtml));
+    $('#about').fadeIn();
   };
 
   module.repoView = repoView;
